e3a804d9c99ec49ffa829eb86c7a2bd1fad366c4
qigaoqiang <qgq3562@163.com>
2022-04-18 00:48:42 +0800

    Add notes on GNU C feature __attribute__

---
+++ __attribute__/doc.md
@@ -0,0 +1,4 @@
+
+参考资料：
+
+[C语言__attribute__](https://blog.csdn.net/essity/article/details/104694694)
---
+++ __attribute__/format-printf.c
@@ -0,0 +1,33 @@
+
+#include <stdio.h>
+#include <stdarg.h>
+
+#ifdef ON
+    // 告诉编译器以printf的方式去检查该函数
+    // gcc -Wall format-printf.c -D ON
+    extern void myprint(const char *fmt, ...) __attribute__((format(printf, 1, 2)));
+#else
+    // gcc -Wall format-printf.c
+    extern void myprint(const char *fmt, ...);
+#endif
+
+void myprint(const char *fmt, ...)
+{
+    va_list ap;
+    va_start(ap, fmt);
+    (void)printf(fmt, ap);
+    va_end(ap);
+}
+
+void test()
+{
+    myprint("number = %d\n", 6);
+    // warning: format ‘%d’ expects argument of type ‘int’, but argument 2 has type ‘char *’ [-Wformat=]
+    myprint("string = %d\n", "hello");
+}
+
+int main(void)
+{
+    test();
+    return 0;
+}
